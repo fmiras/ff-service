a143400b5ec74316344a3bc9c9e925ae10f54a9d
Federico Miras <miras.federico@gmail.com>
2018-05-05 15:03:45 -0300

    Change service name from index.js to service.js

--- test/service.js
+++ test/service.js
@@ -1,7 +1,7 @@
 const test = require('ava')
 const rewire = require('rewire')
 
-const getFeatures = rewire('../src')
+const getFeatures = rewire('../src/service')
 const repository = require('./fixtures/repository')
 
 getFeatures.__set__('repository', repository)
@@ -22,6 +22,17 @@ test('getFeatures of costumer with logging enabled and mfa disabled', t => {
   const expectedFeatures = {
     logging: '2.1.0',
     mfa: false
+  }
+  t.deepEqual(features, expectedFeatures)
+})
+
+test('getFeatures with no parameters, return all enabled features', t => {
+  const customerId = 1
+  const features = getFeatures(customerId)
+  const expectedFeatures = {
+    logging: '2.1.0',
+    roles: '1.7.0',
+    sms: '3.0.0'
   }
   t.deepEqual(features, expectedFeatures)
 })
