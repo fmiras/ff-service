15447306e69e0cefa1ee575ab0d36e7dcfb5bae7
Federico Miras <miras.federico@gmail.com>
2018-05-05 21:10:02 -0300

    Create a database example for mongo setup

---
+++ database-example.js
@@ -0,0 +1,23 @@
+;[
+  {
+    customerId: 1,
+    features: {
+      logging: '2.1.0',
+      roles: '1.7.0',
+      sms: '3.0.0'
+    }
+  },
+  {
+    customerId: 2,
+    features: {
+      mfa: '1.7.1',
+      logging: '1.0.0'
+    }
+  },
+  {
+    customerId: 3,
+    features: {
+      sms: '1.2.2'
+    }
+  }
+]
