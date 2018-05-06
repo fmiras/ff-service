6a3af494017d79092fb451741d3eb8e381b3dad4
Federico Miras <miras.federico@gmail.com>
2018-05-06 02:27:58 -0300

    Create an update repository function for features

--- src/repository.js
+++ src/repository.js
@@ -6,12 +6,26 @@ const find = async query => {
   const connection = await MongoClient.connect(MONGO_URL)
   const db = connection.db(MONGO_DB)
   const collection = db.collection('customers')
-  console.debug(`Querying: ${JSON.stringify(query)}`)
+  console.debug(`Find Querying: ${JSON.stringify(query)}`)
   const result = await collection.findOne(query)
   console.debug(
     `Result for Query ${JSON.stringify(query)} is ${JSON.stringify(result)}`
   )
+  connection.close()
   return result.features
 }
 
-module.exports = { find }
+const update = async ({ customerId, features }) => {
+  const connection = await MongoClient.connect(MONGO_URL)
+  const db = connection.db(MONGO_DB)
+  const collection = db.collection('customers')
+  const result = await collection.update(
+    { customerId },
+    { $set: { features } },
+    { upsert: true }
+  )
+  connection.close()
+  return result
+}
+
+module.exports = { find, update }
